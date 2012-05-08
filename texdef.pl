df0991d56054f7573b8d16107bcc2157f18352a9
Martin Scharrer <martin.scharrer@web.de>
2012-05-08 14:56:02 +0200

    Updated version/date.

--- texdef.pl
+++ texdef.pl
@@ -115,13 +115,13 @@ my $ISCONTEXT = 0;
 my $BEGINENVSTR = '%s';
 my $ENDENVSTR   = '%s';
 
-my $VERSION = 'Version 1.7 -- 2012/05/07';
+my $VERSION = 'Version 1.7a -- 2012/05/08';
 sub usage {
     my $option = shift;
     my $ret    = ($option) ? 0 : 1;
 print << 'EOT';
 texdef -- Show definitions of TeX commands
-Version 1.7 -- 2012/05/07
+Version 1.7a -- 2012/05/08
 Copyright (C) 2011-2012  Martin Scharrer <martin@scharrer-online.de>
 This program comes with ABSOLUTELY NO WARRANTY;
 This is free software, and you are welcome to redistribute it under certain conditions;
